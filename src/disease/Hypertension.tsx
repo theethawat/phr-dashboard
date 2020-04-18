60ce3ee6350094e95d64f3fa7bb4403a3a43f22c
Theethawat Savastham <5910110150@psu.ac.th>
2020-04-18 17:32:45 +0700

    Hypertension Disease Data

--- src/disease/Hypertension.tsx
+++ src/disease/Hypertension.tsx
@@ -9,6 +9,8 @@ class Hypertension extends Component<any, any> {
         this.state = {
             advice: tempAdviceHP,
         }
+        this.renderNewData = this.renderNewData.bind(this)
+        this.updateDatabase = this.updateDatabase.bind(this)
     }
 
     componentDidMount() {
@@ -29,6 +31,38 @@ class Hypertension extends Component<any, any> {
             })
     }
 
+    renderNewData(event: any) {
+        let target = event.target
+        let tempAdvice: IHypertension = {
+            advice_a: (target.name == "optimal" ? target.value : (this.state.advice as IHypertension).advice_a),
+            advice_b: (target.name == "normal" ? target.value : (this.state.advice as IHypertension).advice_b),
+            advice_c: (target.name == "high-normal" ? target.value : (this.state.advice as IHypertension).advice_c),
+            advice_d: (target.name == "ht1" ? target.value : (this.state.advice as IHypertension).advice_d),
+            advice_e: (target.name == "ht2" ? target.value : (this.state.advice as IHypertension).advice_e),
+            advice_f: (target.name == "ht3" ? target.value : (this.state.advice as IHypertension).advice_f),
+            advice_g: (this.state.advice as IHypertension).advice_g,
+            advice_no: (target.name == "tba" ? target.value : (this.state.advice as IHypertension).advice_no),
+            description: (this.state.advice as IHypertension).description
+        }
+        this.setState({
+            advice: tempAdvice
+        })
+    }
+
+    updateDatabase() {
+        let stateAdvice: IHypertension = this.state.advice
+        console.log(stateAdvice)
+        db.collection("disease").doc("hypertension").update({
+            advice_a: stateAdvice.advice_a,
+            advice_b: stateAdvice.advice_b,
+            advice_c: stateAdvice.advice_c,
+            advice_d: stateAdvice.advice_d,
+            advice_e: stateAdvice.advice_e,
+            advice_f: stateAdvice.advice_f,
+            advice_no: stateAdvice.advice_no,
+        }).then(ret => window.alert("Success")).catch(err => window.alert("Error " + err))
+    }
+
     render() {
         let advice: IHypertension = this.state.advice
         console.log(advice)
@@ -39,18 +73,18 @@ class Hypertension extends Component<any, any> {
                 <hr />
                 <div className="card">
                     <div className="card-content">
-                        <form>
+                        <form >
                             <label className="label">
                                 คำแนะนำสำหรับผู้มีความดันโลหิตในเกณฑ์ Optimal (น้อยกว่า 120 และ
                                 น้อยกว่า80)
               </label>
-                            <textarea name="optimal" className="textarea" rows={3} value={advice.advice_a}></textarea>
+                            <textarea name="optimal" className="textarea" rows={3} onChange={this.renderNewData} value={advice.advice_a}></textarea>
 
                             <label className="label">
                                 คำแนะนำสำหรับผู้มีความดันโลหิตในเกณฑ์ Normal (120-129 หรือ
                                 80-84)
               </label>
-                            <textarea name="normal" className="textarea" rows={3} value={advice.advice_b}></textarea>
+                            <textarea name="normal" className="textarea" rows={3} onChange={this.renderNewData} value={advice.advice_b}></textarea>
 
                             <label className="label">
                                 คำแนะนำสำหรับผู้มีความดันโลหิตในเกณฑ์ High Normal (130-139 หรือ
@@ -67,24 +101,26 @@ class Hypertension extends Component<any, any> {
                                 คำแนะนำสำหรับผู้มีความดันโลหิตในเกณฑ์ Possible Hypertension
                                 (140-159 หรือ 90-99)
               </label>
-                            <textarea name="ht1" className="textarea" rows={3} value={advice.advice_d}></textarea>
+                            <textarea name="ht1" className="textarea" rows={3} onChange={this.renderNewData} value={advice.advice_d}></textarea>
 
                             <label className="label">
                                 คำแนะนำสำหรับผู้มีความดันโลหิตในเกณฑ์ Probable hypertension
                                 (160-179หรือ 100-109)
               </label>
-                            <textarea name="ht2" className="textarea" rows={3} value={advice.advice_e}></textarea>
+                            <textarea name="ht2" className="textarea" rows={3} onChange={this.renderNewData} value={advice.advice_e}></textarea>
 
                             <label className="label">
                                 คำแนะนำสำหรับผู้มีความดันโลหิตในเกณฑ์ Definite Hypertension
                                 (>180 หรือ > 110)
               </label>
-                            <textarea name="ht3" className="textarea" rows={3} value={advice.advice_f}></textarea>
+                            <textarea name="ht3" className="textarea" rows={3} onChange={this.renderNewData} value={advice.advice_f}></textarea>
 
                             <label className="label">
                                 คำแนะนำสำหรับผู้ที่ไม่มีข้อมูล หรือ ไม่สามารถให้คำแนะนำได้
               </label>
-                            <textarea name="tba" className="textarea" rows={3} value={advice.advice_no}></textarea>
+                            <textarea name="tba" className="textarea" rows={3} onChange={this.renderNewData} value={advice.advice_no}></textarea>
+                            <br />
+                            <button type="button" onClick={this.updateDatabase} className="button is-primary kanit">ยืนยันการแก้ไขข้อมูล</button>
                         </form>
                     </div>
                 </div>
