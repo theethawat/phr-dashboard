4c12fd3b5d4ef12ff5e14851fd38194928c00c54
Theethawat Savastham <5910110150@psu.ac.th>
2020-05-02 21:48:06 +0700

    Hide Dependencies

--- src/Firebase.js
+++ src/Firebase.js
@@ -1,15 +1,6 @@
 import * as firebase from "firebase/app"
 import "firebase/auth"
 import "firebase/firestore"
-
-const firebaseConfig = {
-  apiKey: "AIzaSyCCjlqtLbEIwqfhHXNIDhXIOOrx3u2IzM8",
-  authDomain: "personal-health-connect-aa36c.firebaseapp.com",
-  databaseURL: "https://personal-health-connect-aa36c.firebaseio.com",
-  projectId: "personal-health-connect-aa36c",
-  storageBucket: "personal-health-connect-aa36c.appspot.com",
-  messagingSenderId: "716479950882",
-  appId: "1:716479950882:web:51df5be3baf2728ae55a7b",
-}
-
+import configFile from "./firebaseConfig.json"
+const firebaseConfig = configFile
 export default firebase.initializeApp(firebaseConfig)
